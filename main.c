05ec86e097fc08756129b95641f58367b35acbf8
Iltotore <rafbodaha@gmail.com>
2022-05-01 11:52:55 +0200

    feat: Add main file.

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "gui.c"
+#include "model.h"
+
+int main() {
+
+  srand(time(NULL));
+
+  Book dummy;
+  dummy.title = "The Scala Cookbook";
+  dummy.author = "Alvin Alexander";
+  dummy.id = 0;
+  dummy.category = Sciences;
+  dummy.borrower = NULL;
+
+  Book books[5];
+  for(int i = 0; i < 5; i++) {
+    books[i] = dummy;
+    books[i].id = rand();
+  }
+
+  showBooks(books, 5);
+  return 0;
+}
