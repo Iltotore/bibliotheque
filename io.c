8ef95c61e21f4ded3282e62e12d573c33db8e3b9
Emessiene Ngouoyong Rachel Emmanuelle <emessienerachel@gmail.com>
2022-05-12 15:21:54 +0200

    fix: Missing model.h

--- io.c
+++ io.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "io.h"
+#include "model.h"
 
 void saveBook(Book a,FILE* text){
   char* username;
--- io.h
+++ io.h
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "model.h"
 #ifndef io
 #define io
 
