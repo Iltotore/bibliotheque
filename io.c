7e2dc22f84f11a74695af0bf2d69865c67421c14
Raphael Fromentin <rafbodaha@gmail.com>
2022-05-17 15:26:26 +0200

    feat: Add loadUser

--- io.c
+++ io.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <time.h>
 #include "io.h"
 #include "model.h"
@@ -24,4 +25,14 @@ Book loadBook(Library lib, FILE* file){
 
 void saveUser(User user, FILE* file) {
   fprintf(file, "%s,%s,%d,%d\n", user.login, user.password, user.role, user.blacklisted);
+}
+
+User loadUser(FILE* file) {
+  User user;
+  int roleId;
+  int blacklisted;
+
+  fscanf(file, "%s,%s,%d,%d\n", user.login, user.password, &roleId, &blacklisted);
+  user.role = (Role) roleId;
+  user.blacklisted = (bool) blacklisted;
 }
--- io.h
+++ io.h
@@ -6,10 +6,13 @@
 //Save the given Book in the given file.
 void saveBook(Book book, FILE* file);
 
-//Load a book from the given file.
+//Load a book from the given file using the passed library.
 Book loadBook(Library library, FILE* file);
 
 //Save the given user in the given file.
 void saveUser(User user, FILE* file);
+
+//Load a user form the given file.
+User loadUser(FILE* file);
 
 #endif
