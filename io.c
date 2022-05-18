d3c917c297e790823ed9de189ab56bcfe564344e
Raphael Fromentin <rafbodaha@gmail.com>
2022-05-18 16:11:08 +0200

    refactor: Rearrange code

--- io.c
+++ io.c
@@ -6,6 +6,25 @@
 #include "model.h"
 #include "util.h"
 
+void saveUser(User user, FILE* file) {
+  fprintf(file, "%s,%s,%d,%d\n", user.login, user.password, user.role, user.blacklisted);
+}
+
+User loadUser(FILE* file) {
+  User user;
+  int roleId;
+  int blacklisted;
+
+  user.login = safeMalloc(sizeof(char)*101);
+  user.password = safeMalloc(sizeof(char)*101);
+
+  fscanf(file, "%100[^,],%100[^,],%d,%d\n", user.login, user.password, &roleId, &blacklisted);
+  user.role = (Role) roleId;
+  user.blacklisted = (bool) blacklisted;
+
+  return user;
+}
+
 void saveBook(Book book, FILE* file){
   fprintf(file,"%s,%s,%d,%d,%s,%ld\n",book.title, book.author, book.id, book.category, book.borrower==NULL ? " " : book.borrower->login, mktime(book.deliveryDate));
  }
@@ -25,25 +44,6 @@ Book loadBook(Library lib, FILE* file){
   book.deliveryDate=localtime(&time);
 
   return book;
-}
-
-void saveUser(User user, FILE* file) {
-  fprintf(file, "%s,%s,%d,%d\n", user.login, user.password, user.role, user.blacklisted);
-}
-
-User loadUser(FILE* file) {
-  User user;
-  int roleId;
-  int blacklisted;
-
-  user.login = safeMalloc(sizeof(char)*101);
-  user.password = safeMalloc(sizeof(char)*101);
-
-  fscanf(file, "%100[^,],%100[^,],%d,%d\n", user.login, user.password, &roleId, &blacklisted);
-  user.role = (Role) roleId;
-  user.blacklisted = (bool) blacklisted;
-
-  return user;
 }
 
 void saveLibrary(Library library, FILE* userFile, FILE* bookFile) {
--- io.h
+++ io.h
@@ -3,17 +3,17 @@
 #ifndef io
 #define io
 
-//Save the given Book in the given file.
-void saveBook(Book book, FILE* file);
-
-//Load a book from the given file using the passed library.
-Book loadBook(Library library, FILE* file);
-
 //Save the given user in the given file.
 void saveUser(User user, FILE* file);
 
 //Load a user from the given file.
 User loadUser(FILE* file);
+
+//Save the given Book in the given file.
+void saveBook(Book book, FILE* file);
+
+//Load a book from the given file using the passed library.
+Book loadBook(Library library, FILE* file);
 
 //Save the given library
 void saveLibrary(Library library, FILE* userFile, FILE* bookFile);
