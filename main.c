980d002562457b508378aa4909f3bb8f3fc4fdab
Iltotore <rafbodaha@gmail.com>
2022-05-22 14:27:08 +0200

    feat: Add registration menu

--- main.c
+++ main.c
@@ -1,9 +1,97 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "model.h"
 #include "gui.h"
 #include "io.h"
 #include "util.h"
+
+#define UPPERS "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+#define LOWERS "abcdefghijklmnopqrstuvwxyz"
+#define DIGITS "123456789"
+
+User* loginMenu(Library library) {
+  int i=0;
+  User* user;
+  do{
+    if(i==3){
+      printf("Le nombre maximal d'essais a été atteint.\n");
+      return 0;
+    }
+    char* login = safeMalloc(sizeof(char)*100);
+    char* password =safeMalloc(sizeof(char)*100);
+    printf("Saisissez votre nom d'utilisateur :\n");
+    scanf("%100s",login);
+    clear(stdin);
+    printf("Saisissez votre mot de passe :\n");
+    scanf("%100s",password);
+    clear(stdin);
+    user = authenticateUser(library,login,password);
+    if (user == NULL) printf("Vos identifiants sont incorrects, veuillez réessayer.\n");
+    i++;
+  }while( user == NULL);
+
+  return user;
+}
+
+User* registerMenu(Library* library) {
+  User* existing = NULL;
+  char* login = safeMalloc(sizeof(char)*101);
+  do {
+    printf("Saisissez votre nom d'utilisateur :\n");
+    scanf("%100s", login);
+    clear(stdin);
+    existing = getUser(*library, login);
+    if(existing != NULL) printf("Ce nom est déjà pris.\n");
+  } while(existing != NULL);
+
+  char* password = safeMalloc(sizeof(char)*101);
+  bool valid = false;
+  do {
+    valid = true;
+    printf("Saisissez votre mot de passe:\n");
+    scanf("%100s", password);
+    clear(stdin);
+    if(strlen(password) < 8) {
+      printf("Le mot de passe doit avoir une longueur minimale de 8.\n");
+      valid = false;
+    }
+
+    if(!strContainsAtLeast(password, UPPERS)) {
+      printf("Le mot de passe doit contenir au moins une majuscule.\n");
+      valid = false;
+    }
+
+    if(!strContainsAtLeast(password, LOWERS)) {
+      printf("Le mot de passe doit contenir au moins une minuscule.\n");
+      valid = false;
+    }
+
+    if(!strContainsAtLeast(password, DIGITS)) {
+      printf("Le mot de passe doit contenir au moins un chiffre.\n");
+      valid = false;
+    }
+
+    if(valid) {
+      char* confirm = safeMalloc(sizeof(char)*101);
+      printf("Confirmez votre mot de passe:\n");
+      scanf("%100s", confirm);
+      printf("Scanned\n");
+      clear(stdin);
+      printf("Cleared\n");
+      if(strcmp(password, confirm) != 0) {
+        printf("Les deux mots de passe ne correspondent pas.\n");
+        valid = false;
+      }
+    }
+  } while(!valid);
+
+  printf("Création...\n");
+  User* user = registerUser(library, login, password);
+  printf("Votre compte a bien été créé.\n");
+
+  return user;
+}
 
 int main() {
   srand(time(NULL));
@@ -21,29 +109,22 @@ int main() {
   fclose(bookFile);
   fclose(userFile);
   User* user;
-  int i=0;
-  //Authentification
-  do{
-    if(i==3){
-      printf("Le nombre maximal d'essais a été atteint.\n");
-      return 0;
-    }
-    char* login = safeMalloc(sizeof(char)*100);
-    char* password =safeMalloc(sizeof(char)*100);
-    printf("Rentrez votre nom d'utilisateur : \n");
-    scanf("%100s",login);
-    clear(stdin);
-    printf("Rentrez votre mot de passe : \n");
-    scanf("%100s",password);
-    clear(stdin);
-    user = authenticateUser(library,login,password);
-    if (user == NULL) printf("Vos identifiants sont incorrects, veuillez réessayer.\n");
-    i++;
-  }while( user == NULL);
+
+  char* logChoices[2] = {"Se connecter", "S'inscrire"};
+
+  switch(askInt("Bonjour !", logChoices, 2)) {
+    case 0:
+      user = loginMenu(library);
+      break;
+    case 1:
+      user = registerMenu(&library);
+      break;
+  }
+
   char* choices[2]={"Les livres de la bibliothèque","Quitter"};
   int action;
   do{
-    action =askInt("Sélectionnez une action", choices, 2);
+    action = askInt("Sélectionnez une action", choices, 2);
     switch (action) {
       case 0:
        showBooks(library.books, library.bookCount, NO_FIELD);
